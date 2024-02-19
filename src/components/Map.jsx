cd80f57f87fc0292d145fe7c42d291d91b921d9e
Amirhossein <4mirh03einhashemi@gmail.com>
2024-02-19 21:05:08 +0330

    🔨 set params and query string in url

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,15 @@
+import { useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
 function Map() {
-  return <div className={styles.mapContainer}></div>;
+  const [searchQuery, setSearchQuery] = useSearchParams();
+  const lat = searchQuery.get("lat");
+  const lng = searchQuery.get("lng");
+  return (
+    <div className={styles.mapContainer}>
+      position: {lat} , {lng}
+    </div>
+  );
 }
 
 export default Map;
