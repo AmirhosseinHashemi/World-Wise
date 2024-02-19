339a1815ea6e2499ca50162139dc01a3649e6138
Amirhossein <4mirh03einhashemi@gmail.com>
2024-02-20 01:40:24 +0330

    🔨 implement programmatic navigate

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,12 +1,15 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
 function Map() {
   const [searchQuery, setSearchQuery] = useSearchParams();
   const lat = searchQuery.get("lat");
   const lng = searchQuery.get("lng");
+
+  const navigate = useNavigate();
+
   return (
-    <div className={styles.mapContainer}>
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
       position: {lat} , {lng}
     </div>
   );
