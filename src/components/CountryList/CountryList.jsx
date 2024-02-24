889f04ccf5cce0aa19206aa373686b32edbee1a2
Amirhossein <4mirh03einhashemi@gmail.com>
2024-02-24 19:54:09 +0330

    ♻ use context for store data and create custom hook

--- src/components/CountryList/CountryList.jsx
+++ src/components/CountryList/CountryList.jsx
@@ -2,8 +2,11 @@ import CountryItem from "../CountryItem/CountryItem";
 import Spinner from "../Spinner/Spinner";
 import Message from "../Message/Message";
 import styles from "./CountryList.module.css";
+import { useCities } from "../../contexts/CitiesContext";
 
-function CountryList({ cities, isLoading }) {
+function CountryList() {
+  const { cities, isLoading } = useCities();
+
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
