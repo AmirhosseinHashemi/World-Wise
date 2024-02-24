7e3533c6ca6dfbe6ab8e3112c2cded7d347929c1
Amirhossein <4mirh03einhashemi@gmail.com>
2024-02-24 23:31:30 +0330

    🔨 develop Map component

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,18 +1,58 @@
+import { useEffect, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import { useCities } from "../../contexts/CitiesContext";
 import styles from "./Map.module.css";
 
 function Map() {
-  const [searchQuery, setSearchQuery] = useSearchParams();
-  const lat = searchQuery.get("lat");
-  const lng = searchQuery.get("lng");
+  const { cities } = useCities();
+  const [mapPosition, setMapPosition] = useState([51.505, -0.09]);
+  const [searchQuery] = useSearchParams();
+  const mapLat = searchQuery.get("lat");
+  const mapLng = searchQuery.get("lng");
 
-  const navigate = useNavigate();
+  useEffect(
+    function () {
+      if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+    },
+    [mapLat, mapLng]
+  );
+
+  // const navigate = useNavigate();
 
   return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      position: {lat} , {lng}
+    <div className={styles.mapContainer}>
+      <MapContainer
+        className={styles.map}
+        center={mapPosition}
+        zoom={6}
+        scrollWheelZoom={true}
+        zoomControl={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker
+            key={city.id}
+            position={[city.position.lat, city.position.lng]}
+          >
+            <Popup>
+              <span>{city.emoji}</span> <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+        <ChangeMap position={mapPosition} />
+      </MapContainer>
     </div>
   );
+}
+
+function ChangeMap({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
 }
 
 export default Map;
