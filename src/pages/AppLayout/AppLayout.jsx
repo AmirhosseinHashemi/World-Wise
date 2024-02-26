5ed5b385927eb461af28e71a52645c44fd2c335a
Amirhossein <4mirh03einhashemi@gmail.com>
2024-02-26 23:41:05 +0330

    🏗 implement fake authentication

--- src/pages/AppLayout/AppLayout.jsx
+++ src/pages/AppLayout/AppLayout.jsx
@@ -1,12 +1,17 @@
 import Map from "../../components/Map/Map";
 import Sidebar from "../../components/Sidebar/Sidebar";
+import User from "../../components/User/User";
+import { useAuth } from "../../contexts/FakeAuthContext";
 import styles from "./AppLayout.module.css";
 
 function AppLayout() {
+  const { isAuthenticated } = useAuth();
+
   return (
     <div className={styles.app}>
       <Sidebar />
       <Map />
+      <User />
     </div>
   );
 }
